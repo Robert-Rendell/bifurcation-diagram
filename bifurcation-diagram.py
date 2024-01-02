461899893e523901f803bee8f4b104bbd7d7049b
rob_452@hotmail.co.uk <robertrendell@hotmail.com>
2024-01-02 10:34:34 +0000

    Python3 implementation of Bifurcation diagram

---
+++ bifurcation-diagram.py
@@ -0,0 +1,24 @@
+# Credits:
+#  - https://stackoverflow.com/questions/47021057/bifurcation-diagram-with-python
+#  - https://stackoverflow.com/users/1245372/soroush
+
+# Bifurcation diagram of the logistic map
+
+from PIL import Image
+imgx = 1000
+imgy = 500
+image = Image.new("RGB", (imgx, imgy))
+
+xa = 2.9
+xb = 4.0
+maxit = 1000
+
+for i in range(imgx):
+    r = xa + (xb - xa) * float(i) / (imgx - 1)
+    x = 0.5
+    for j in range(maxit):
+        x = r * x * (1 - x)
+        if j > maxit / 2:
+            image.putpixel((i, int(x * imgy)), (255, 255, 255))
+
+image.save("Bifurcation.png", "PNG")
